222921b28d0953f457da84535358ca527655302b
selosele <osh3384@naver.com>
2023-09-30 14:23:49 +0900

    cmd 디렉터리 삭제

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/selosele/nancy/cmd"
+import (
+	"log"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/joho/godotenv"
+	"github.com/selosele/nancy/handlers"
+)
 
 func main() {
-	cmd.Init()
+
+	// 환경변수 로드
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Cloudinary 클라이언트 초기화
+	cld, err := cloudinary.New()
+	if err != nil {
+		log.Fatalf("Failed to initialize Cloudinary, %v\n", err)
+	}
+
+	// 라우트 설정
+	handlers.Init(cld)
 }
