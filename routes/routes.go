0c3bb36183b2f530dbcfb4b801e16de138bea45c
selosele <osh3384@naver.com>
2023-10-07 12:47:07 +0900

    인증 미들웨어 작업

--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,7 @@ import (
 	"github.com/selosele/nancy/handlers/remove"
 	"github.com/selosele/nancy/handlers/search"
 	"github.com/selosele/nancy/handlers/upload"
+	"github.com/selosele/nancy/middlewares"
 	"github.com/selosele/nancy/models"
 )
 
@@ -15,22 +16,23 @@ import (
 func Setup(p models.HandlerParams) {
 	router := mux.NewRouter()
 	apiRouter := router.PathPrefix("/api/").Subrouter()
+	apiRouter.Use(middlewares.AuthMiddleware)
 
 	// 파일 검색 Handler
 	searchHandler := search.Handler{Params: p}
-	apiRouter.HandleFunc("/files", searchHandler.HandleRequest).Methods(http.MethodGet)
+	apiRouter.HandleFunc("/files", searchHandler.HandleRequest).Methods("GET")
 
 	// 파일 업로드 Handler
 	uploadHandler := upload.Handler{Params: p}
-	apiRouter.HandleFunc("/files", uploadHandler.HandleRequest).Methods(http.MethodPost)
+	apiRouter.HandleFunc("/files", uploadHandler.HandleRequest).Methods("POST")
 
 	// 파일 삭제 Handler
 	removeHandler := remove.Handler{Params: p}
-	apiRouter.HandleFunc("/files", removeHandler.HandleRequest).Methods(http.MethodDelete)
+	apiRouter.HandleFunc("/files", removeHandler.HandleRequest).Methods("DELETE")
 
 	// 폴더 목록 조회 Handler
 	foldersHandler := folders.Handler{Params: p}
-	apiRouter.HandleFunc("/folders", foldersHandler.HandleRequest).Methods(http.MethodGet)
+	apiRouter.HandleFunc("/folders", foldersHandler.HandleRequest).Methods("GET")
 
 	http.Handle("/", router)
 	http.ListenAndServe(":5000", nil)
